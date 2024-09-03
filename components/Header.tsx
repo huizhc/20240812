70af8a8c656e183ee792d0becb646f5d52170149
huizhc <1125133876@qq.com>
2024-09-04 00:26:19 +0800

    移动端优化

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,52 @@
+"use client";
 
 import Image from "next/image";
+import HamburgerSvg from "../public/Hamburger.svg";
+import CloseSvg from "../public/close.svg";
+import { useState } from "react";
 
-export default function Header () {
+export default function Header() {
+  const [visibled, setVisibled] = useState(false);
+
+  const handleVisible = () => {
+    setVisibled(!visibled);
+  };
+
   return (
     <header>
       <div className="leftBox">
         <Image src="/logo.svg" alt="logo" width={32} height={18} priority />
         PDF.ai
       </div>
-      <div className="rightBox">
-        <a className="hover:underline">Pricing</a>
-        <a className="hover:underline">Chrome extension</a>
-        <a className="hover:underline">Use cases</a>
-        <a className="hover:underline">Get started →</a>
+      <div className="rightBox hide-mobile">
+        <a className="hover:underline" href="/pricing">
+          Pricing
+        </a>
+        <a className="hover:underline" href="/chrome-extension">
+          Chrome extension
+        </a>
+        <a className="hover:underline" href="/use-cases">
+          Use cases
+        </a>
+        <a className="hover:underline" href="/auth/sign-in">
+          Get started →
+        </a>
       </div>
+      <button className="ml-auto p-2.5 hide-desktop" onClick={handleVisible}>
+        {visibled ? (
+          <CloseSvg width={20} height={20} />
+        ) : (
+          <HamburgerSvg width={20} height={20} />
+        )}
+      </button>
+      {visibled && (
+        <div className="foldBox hide-desktop">
+          <a href="/pricing">Pricing</a>
+          <a href="/chrome-extension">Chrome extension</a>
+          <a href="/use-cases">Use cases</a>
+          <a href="/auth/sign-in">Get started →</a>
+        </div>
+      )}
     </header>
   );
-};
+}
